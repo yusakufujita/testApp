2809478f4b4f5be7e8a205832be536fd287c5fbb
藤田優作 <marozaemon1217@gmail.com>
2020-10-25 06:48:13 +0900

    first commit

---
+++ testApp/CustomAuthViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomAuthViewController.swift
+//  testApp
+//
+//  Created by 藤田優作 on 2020/10/16.
+//
+
+import UIKit
+
+class CustomAuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
